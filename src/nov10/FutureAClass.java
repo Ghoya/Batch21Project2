23ac8641a24a8a45b129cff978522fc19ec61dc8
Ghoya <58498238+Ghoya@users.noreply.github.com>
2020-11-10 13:48:02 -0800

    we taked out .idea

---
+++ src/nov10/FutureAClass.java
@@ -0,0 +1,9 @@
+package nov10;
+
+public class FutureAClass {
+    public static void main(String[] args) {
+        System.out.println("\"What should I do?\" = " + "What should I do?");
+
+    }
+
+}
