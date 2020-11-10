2ca82c4a231ab3c77e8077cc0b54dc927caf4b4c
Ghoya <58498238+Ghoya@users.noreply.github.com>
2020-11-10 13:50:37 -0800

    we are addding  new

--- src/nov10/FutureAClass.java
+++ src/nov10/FutureAClass.java
@@ -3,7 +3,7 @@ package nov10;
 public class FutureAClass {
     public static void main(String[] args) {
         System.out.println("\"What should I do?\" = " + "What should I do?");
-
+// we are doing work
     }
 
 }
